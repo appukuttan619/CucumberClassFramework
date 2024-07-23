7ed82546b9fba2ad321c6bf1e6413f1bdd8ee101
appukuttan619 <147173733+appukuttan619@users.noreply.github.com>
2024-07-23 10:12:20 +0530

    Update TestRunner.java
    editing the runner file

--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.automation.steps"
-        // ,
+        glue = "com.automation.steps",
+        plugin = {"html:targe/report.html","json:targe/cucumber.json"}
         // tags = "@WIP",
         // dryRun = true
 )
